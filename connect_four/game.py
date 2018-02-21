b5472336c8451f21778e1a12a95bb3f657b6e707
Aly Sivji <alysivji@gmail.com>
2018-02-21 14:53:04 -0600

    Drop piece and add check for winning condition

--- connect_four/game.py
+++ connect_four/game.py
@@ -2,9 +2,15 @@
 
 PLAYER_ONE = '🔴'
 PLAYER_TWO = '🔵'
+EMPTY = ' '
 
 NUM_COLS = 7
 NUM_ROWS = 6
+
+
+def all_same(items):
+    return (items[0] != EMPTY and
+            all(x == items[0] for x in items))
 
 
 class Board:
@@ -21,7 +27,7 @@ class Board:
         for col in (range(NUM_COLS)):
             col_dict = {}
             for row in range(NUM_ROWS):
-                col_dict[row] = ' '
+                col_dict[row] = EMPTY
             board[col] = col_dict
         self._grid = board
 
@@ -49,6 +55,67 @@ class Board:
 
     def __repr__(self):
         return self.draw_board()
+
+    def drop_piece(self, selected_col, piece):
+        """Given a column number, put the piece in the first free slot"""
+
+        try:
+            col = self._grid[selected_col]
+        except KeyError:
+            print('Not a valid key!')
+            return False
+
+        free_slot = None
+        for key, item in col.items():
+            if item == EMPTY:
+                free_slot = key
+                break
+
+        if free_slot is None:
+            print('Column is full!')
+            return False
+
+        col[free_slot] = piece
+        return True
+
+    def check_win(self, position):
+        """Given the position of a piece, check to see if there is a possible
+        four in a row on the board"""
+
+        # victory positions can go in 8 directions, get them all
+        def calculate_line(pos, col_offset: int, row_offset: int):
+            col, row = pos
+            streak = [pos]
+            for count in range(1, 4):
+                streak.append((col + count * col_offset,
+                               row + count * row_offset))
+            return streak
+
+        # cycle thru each direction and got possible streak paths
+        possible_winning_lines = []
+        possible_winning_lines.append(calculate_line(position, 0, 1))  # N
+        possible_winning_lines.append(calculate_line(position, 1, 1))  # NE
+        possible_winning_lines.append(calculate_line(position, 1, 0))  # E
+        possible_winning_lines.append(calculate_line(position, 1, -1))  # SE
+        possible_winning_lines.append(calculate_line(position, 0, -1))  # S
+        possible_winning_lines.append(calculate_line(position, -1, -1))  # SW
+        possible_winning_lines.append(calculate_line(position, -1, 0))  # W
+        possible_winning_lines.append(calculate_line(position, -1, 1))  # NW
+
+        for line in possible_winning_lines:
+            try:
+                board_values = [self._grid[col][row] for col, row in line]
+            except KeyError:
+                print('Bad ', line)
+                continue
+
+            print('Good ', line)
+            if all_same(board_values):
+                print('Winner! Winner! Chicken Dinner!')
+                print(f'{board_values[0]} wins!')
+                return True
+
+        return False
 
 
 if __name__ == '__main__':
--- tests/game_test.py
+++ tests/game_test.py
@@ -1,5 +1,5 @@
 """Tests for Connect Four"""
-from connect_four.game import Board
+from connect_four.game import Board, NUM_ROWS
 
 
 def test_draw_board_without_errors():
@@ -12,3 +12,15 @@ def test_draw_board_without_errors():
     # Assert
     # if we are here, we didn't have an error so that's good
     assert True
+
+
+def test_fill_board_column():
+    # Arange
+    game = Board()
+
+    # Act and Assert
+    for _ in range(NUM_ROWS):
+        assert game.drop_piece(1, 'x') is True
+
+    # full, now we cannot
+    assert game.drop_piece(1, 'x') is False
